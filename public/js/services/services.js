ca64309ef9a08ee745a9e19468d2056d3ee0048c
Katherine Mattison <kbmedisto@gmail.com>
2014-05-23 15:55:52 -0400

    chnged parenthesis in service app

--- public/js/services/services.js
+++ public/js/services/services.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('ngDay2App')
 	.factory('PostsSvc', function($resource) {
-		return $resource('api/collections/protected-garden-8476',
+		return $resource('api/collections/protected-garden-8476'),
 			{},
 			{
 				query: { method: 'GET', isArray: true },
@@ -9,7 +9,7 @@ angular.module('ngDay2App')
 			});
 	})
 	.factory('PostSvc', function($resource) {
-		return $resource('api/collections/protected-garden-8476/:id',
+		return $resource('api/collections/protected-garden-8476/:id'),
 			{
 				id: '@_id'
 			},
